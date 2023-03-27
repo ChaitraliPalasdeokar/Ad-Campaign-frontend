71b7948e3d6379153039c8109a5d68e0c000ae6e
chaitralipalasdeokarc <chaitrali.p@thoughtworks.com>
2023-03-27 13:05:50 +0530

    Refactor bar chart

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,11 +1,7 @@
 import React from "react";
 import { Bar } from "react-chartjs-2";
-// import data from "./data";
-import { Chart as ChartJS } from 'chart.js/auto'
 
 const BarChart = ({data}) => {
-	// const labels = data.map((item) => item.label);
-	// const values = data.map((item) => item.value);
 
 	const chartData = {
 		labels: Object.keys(data),
@@ -18,24 +14,9 @@ const BarChart = ({data}) => {
 		],
 	};
 
-	// const chartOptions = {
-	// 	responsive: true,
-	// 	maintainAspectRatio: false,  
-	// 	// scales: {
-	// 	// 	yAxes: [
-	// 	// 		{
-					  
-	// 	// 		},
-	// 	// 	],
-	// 	// },
-	// };
-
 	const chartOptions = {
 		responsive: true,
 		plugins: {
-    // legend: {
-    //   position: 'top',
-    // },
     title: {
       display: true,
       text: 'Attributed Revenue for each source',
